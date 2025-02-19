7039adb3ad5e615941d3e4e894797b007537e236
Dilum Aluthge <dilum@aluthge.com>
2025-02-18 21:14:02 -0500

    Comment out some code that seems to be unused

--- src/ElasticClusterManager.jl
+++ src/ElasticClusterManager.jl
@@ -14,12 +14,14 @@ using Distributed: launch, manage, kill, init_worker, connect
 export launch, manage, kill, init_worker, connect
 export ElasticManager, elastic_worker
 
-function worker_cookie()
-  Distributed.init_multi()
-  return Distributed.cluster_cookie()
-end
+# This seems to be currently unused:
+# function worker_cookie()
+#   Distributed.init_multi()
+#   return Distributed.cluster_cookie()
+# end
 
-worker_arg() = `--worker=$(worker_cookie())`
+# This seems to be currently unused:
+# worker_arg() = `--worker=$(worker_cookie())`
 
 include("elastic.jl")
 
